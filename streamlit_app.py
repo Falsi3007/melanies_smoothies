342d159f1927c337bd40b820cc3dba0bb0accbc3
Falsi Vadaliya <110164717+Falsi3007@users.noreply.github.com>
2024-12-28 08:29:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -14,14 +13,9 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-pd_pf = my_dataframe.to_pandas()
-# st.dataframe(pd_pf)
-# st.stop()
-
+# Multiselect widget for ingredients
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
@@ -29,20 +23,12 @@ ingredients_list = st.multiselect(
 )
 
 # Initialize `ingredients_string` to an empty string
-
+ingredients_string = ''
 
 if ingredients_list:
-    ingredients_string = ''
+    # Construct the ingredients string from the selected fruits
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-
 
 # Construct the SQL insert statement
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -55,4 +41,3 @@ if time_to_insert:
     # Execute the SQL insert statement
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
-
