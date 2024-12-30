2d8d08ae787943745b7b2b93f37af8a6edc599dd
Vadaliya Falsi Hareshbhai <110164717+Falsi3007@users.noreply.github.com>
2024-12-30 12:01:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on = pd_pf.loc[pd_pf['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.show(search_on)
         st.write(f"The search value for {fruit_chosen} is {search_on}.")
 
         # Fetch nutrition information from API
