8cba3cf4a3aabcb2a2c351b4a9ce5ac0173b5426
Vadaliya Falsi Hareshbhai <110164717+Falsi3007@users.noreply.github.com>
2024-12-30 10:55:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ if ingredients_list:
     # Construct the ingredients string from the selected fruits
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 # Construct the SQL insert statement
